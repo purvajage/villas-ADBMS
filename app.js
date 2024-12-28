749c9b97d8605a39ea1c4b2fa8762d128452b88f
purva jage <purvajage44@gmail.com>
2024-12-28 13:50:23 +0530

    commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const contactRoutes = require('./routes/contactroutes');
 const scheduleRoutes = require('./routes/scheduleroutes');
-
+const auth=require("./routes/auth");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +14,11 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/vendor', express.static(path.join(__dirname, 'vendor')));
+app.use('/middleware', express.static(path.join(__dirname, 'middleware')));
+app.use('/models', express.static(path.join(__dirname, 'models')));
+app.use('/routes', express.static(path.join(__dirname, 'routes')));
+app.use('/vendor', express.static(path.join(__dirname, 'vendor')));
+
 
 mongoose.connect('mongodb://localhost:27017/villas')
     .then(() => {
@@ -24,6 +29,7 @@ mongoose.connect('mongodb://localhost:27017/villas')
     });
 
 // Use the routes
+app.use(auth)
 app.use(contactRoutes);
 app.use(scheduleRoutes);
 
